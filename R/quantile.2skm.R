6746a1905e16b62c524b7272935b96ffaf3e2959
pedrohcgs <pedrohcgs@gmail.com>
2016-12-01 20:00:41 -0600

    Allow for defective distribution In such case, not all quantiles are well-defined

--- R/quantile.2skm.R
+++ R/quantile.2skm.R
@@ -4,7 +4,7 @@ quantile.2skm <- function(F.ecdf, probs = c(0, 0.25, 0.5, 0.75, 1)) {
   x <- x[order(x)]                              #here
   quantiles <- vapply(probs, FUN = function(p) {x[which(p <= F.ecdf(x))[1]]}, FUN.VALUE = 1.0)
   #correct small amount at very top of distribution (deffective distributions).
-  quantiles[is.na(quantiles)==1] <- x[which(F.ecdf(x) == max(F.ecdf(x)))[1]]
+  #quantiles[is.na(quantiles)==1] <- x[which(F.ecdf(x) == max(F.ecdf(x)))[1]]
 
   nams <- paste(format(round(probs * 100,
                 if (length(probs) > 1) 2 - log10(diff(range(probs))) else 2)),
